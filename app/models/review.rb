6e3f551ebf57cf1ccf8765181d3d8ff7c1e13ef5
jackymeslin <jackymeslin@hotmail.com>
2020-04-01 15:33:30 +0200

    routes OK

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,8 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: [0...5],
-    message: "%{value} is not a valid rating" }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5).to_a }
 
 end
+
+# message: "%{value} is not a valid rating"
